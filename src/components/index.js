e05234160dd95dcd5a91a017c2e568cebb4d97ef
Rahul <rahulhero93111@gmail.com>
2024-03-04 12:04:17 +0530

    pages done

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,11 @@ import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import RTE from "./RTE.jsx";
+import Signup from "./Signup";
+import  login  from "./Login.jsx";
+import Post from  "./Post" ;
+import PostForm from  './post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx'
 
 export {
     Header,
@@ -11,5 +16,10 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    login,
+    Post,
+    PostForm,
+    PostCard
 }
